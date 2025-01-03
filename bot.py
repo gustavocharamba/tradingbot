2c01af201859056997c2a518ec5a51f8406d8346
Gustavo Charamba <gucharamba@gmail.com>
2025-01-03 13:27:30 -0300

    rsi

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import yfinance as yf
 
+from tradingbot.indicators.rsi import __getRSI__
+
 data = pd.read_csv('datas/BTC1H2018-2024.csv')
+
+rsi = __getRSI__(data, 9)
+
+print(rsi)
 
 
 
---
+++ indicators/rsi.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+def __getRSI__(history, period):
+
+    # Calculate price changes
+    delta = history['Close'].diff()
+
+    # Separate gains and losses
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+
+    # Calculate exponential moving averages of gains and losses
+    avg_gain = gain.ewm(span=period, min_periods=period).mean()
+    avg_loss = loss.ewm(span=period, min_periods=period).mean()
+
+    # Calculate Relative Strength (RS)
+    rs = avg_gain / avg_loss
+
+    # Calculate RSI
+    rsi = 100 - (100 / (1 + rs))
+
+    # Calculate short-term SMA of RSI (RSI Signal)
+    rsi_signal = rsi.rolling(window=3).mean()
+
+    return pd.DataFrame({
+        'RSI': rsi,
+        'RSI_Signal': rsi_signal,
+    })
